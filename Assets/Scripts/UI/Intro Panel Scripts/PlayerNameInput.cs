98a409687580060b1ffccc5d10044b2de521b8ce
karan patidar <karan.patidar@wmgmingstudioz.com>
2026-03-18 15:39:25 +0530

    update : added  exit button in user panel

--- Assets/Scripts/UI/Intro Panel Scripts/PlayerNameInput.cs
+++ Assets/Scripts/UI/Intro Panel Scripts/PlayerNameInput.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerNameInput : MonoBehaviour
 {
@@ -8,29 +9,60 @@ public class PlayerNameInput : MonoBehaviour
     [SerializeField] private TMP_InputField firstNameInputField;
     [SerializeField] private TMP_InputField lastNameInputField;
     [SerializeField] private Button startGameButton;
+
     [Header("UI")]
     [SerializeField] private TMP_Text titleText;
+    [SerializeField] private Button exitButton;
 
     [SerializeField] private GameObject userPanel;
     [SerializeField] private GameObject mainMenuPanel;
-   
+    [SerializeField] private GameObject introPanel;
 
     private void Start()
     {
         startGameButton.onClick.AddListener(OnStartGameClicked);
-       
+        exitButton.onClick.AddListener(OnExitClicked);
     }
 
     public void OpenNewPlayer()
     {
         titleText.text = "Create a new account";
-        userPanel.SetActive(true);
+        OpenUserPanel();
     }
 
     public void OpenPreviousPlayer()
     {
         titleText.text = "Enter user detail";
-        userPanel.SetActive(true);
+        OpenUserPanel();
+    }
+
+    private void OpenUserPanel()
+    {
+        // Scale down introPanel first, then open userPanel
+        introPanel.transform.DOScale(0, 0.2f).OnComplete(() =>
+        {
+            introPanel.SetActive(false);
+
+            userPanel.SetActive(true);
+            userPanel.transform.localScale = Vector3.zero;
+            userPanel.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+        });
+    }
+
+    private void OnExitClicked()
+    {
+        firstNameInputField.text = "";
+        lastNameInputField.text = "";
+
+        // Scale down userPanel first, then open introPanel
+        userPanel.transform.DOScale(0, 0.2f).OnComplete(() =>
+        {
+            userPanel.SetActive(false);
+
+            introPanel.SetActive(true);
+            introPanel.transform.localScale = Vector3.zero;
+            introPanel.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+        });
     }
 
     private void OnStartGameClicked()
@@ -45,11 +77,16 @@ public class PlayerNameInput : MonoBehaviour
         }
 
         string fullName = firstName + " " + lastName;
-
         PlayerPrefs.SetString("PlayerName", fullName);
 
-        userPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);
+        // Scale down userPanel first, then open mainMenuPanel
+        userPanel.transform.DOScale(0, 0.2f).OnComplete(() =>
+        {
+            userPanel.SetActive(false);
+
+            mainMenuPanel.SetActive(true);
+            mainMenuPanel.transform.localScale = Vector3.zero;
+            mainMenuPanel.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+        });
     }
-    
 }
