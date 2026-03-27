d4447f3293b8fbc68eb8590afbfad8f28378e685
deepakkalyani355 <deepak.kalyani@wmgamingstudioz.com>
2026-03-27 17:28:15 +0530

    update : 3 camera setup

---
+++ Assets/Scripts/Game/MultiDisplayTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDisplayTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+        Debug.LogWarning("Detected display : "+ Display.displays.Length);
+    }
+}
