c82e38588c901c23f457d7cefe8f4b9354b47624
VishnuWMGaming <vishnu.raveendran@wmgamingstudioz.com>
2026-08-17 13:16:54 +0530

    Update: Added the new models

--- Assets/Scripts/Game/WindManager.cs
+++ Assets/Scripts/Game/WindManager.cs
@@ -32,10 +32,10 @@ public class WindManager : MonoBehaviour
     {
         attractionForce = level switch
         {
-            DifficultyLevel.easy => 6f,
-            DifficultyLevel.medium => 12f,
+            DifficultyLevel.easy => 10f,
+            DifficultyLevel.medium => 15f,
             DifficultyLevel.hard => 20f,
-           _ => 6f
+           _ => 10f
         };
     }
 
